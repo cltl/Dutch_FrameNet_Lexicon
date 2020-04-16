08776d98488cbf3b5e372036319fdd8834c6e3ac
MartenPostma <martenp@gmail.com>
2020-04-16 17:05:56 +0200

    created input for annotation tool

--- lib/evaluation.py
+++ lib/evaluation.py
@@ -1,7 +1,8 @@
-import os
-import sys
+import json
+from pathlib import Path
 import pickle
 from shutil import rmtree
+import subprocess
 
 import graph_utils
 
@@ -10,6 +11,7 @@ import networkx as nx
 def create_svg_from_rbn_sense_to_fn_frame(rbn_sense,
                                           fn_lu,
                                           g,
+                                          index,
                                           output_folder,
                                           verbose=0):
     """
@@ -21,9 +23,11 @@ def create_svg_from_rbn_sense_to_fn_frame(rbn_sense,
     -../output/dfn_objects/g.p
     -../output/dfn_objects/g_v1.p
     -etc.
+    :param int index: used a basename for output path
+    :param output_folder: output folder
     :return:
     """
-    output_path_without_svg = f'{output_folder}/{fn_lu}'
+    output_path_without_svg = f'{output_folder}/{index}'
     from_node = rbn_sense
     to_node = fn_lu
 
@@ -53,10 +57,11 @@ def create_svg_from_rbn_sense_to_fn_frame(rbn_sense,
         print()
         print(f'stored svg image at {output_path_without_svg}.svg')
 
-def create_svg_folder_rbn_sense_to_fn_lus(output_folder,
-                                          graph_path,
-                                          path_rbn_to_lu,
-                                          verbose=0):
+def create_annotation_folder(output_folder,
+                             graph_path,
+                             path_rbn_to_lu,
+                             overwrite=False,
+                             verbose=0):
     """
 
     :param str output_folder: path to output folder (will be removed if exists)
@@ -68,9 +73,18 @@ def create_svg_folder_rbn_sense_to_fn_lus(output_folder,
     :param str path_rbn_to_lu: path to mapping from RBN sense to FN LU ids, e.g,
      'r_n-38434': {'LU-5962'}, 'r_n-31138': {'LU-5962'}
     """
-    if os.path.exists(output_folder):
-        rmtree(output_folder)
-    os.mkdir(output_folder)
+    output_folder = Path(output_folder)
+
+    if overwrite:
+        if output_folder.exists():
+            rmtree(str(output_folder))
+
+    images_folder = output_folder.joinpath('images')
+    annotations_folder = output_folder.joinpath('annotations')
+    if not output_folder.exists():
+        output_folder.mkdir(parents=True)
+        images_folder.mkdir()
+        annotations_folder.mkdir()
 
     g = nx.read_gpickle(graph_path)
     rbn_sense_to_fn_lu = pickle.load(open(path_rbn_to_lu, 'rb'))
@@ -81,8 +95,10 @@ def create_svg_folder_rbn_sense_to_fn_lus(output_folder,
             sense_id = g.nodes[node]['attr']['sense_id']
             sense_id_to_sense_label[sense_id] = node
 
-    svgs = 0
-    for rbn_sense, set_of_lu_ids in rbn_sense_to_fn_lu.items():
+    index_to_edge = {}
+    index_to_annotation = {}
+    for index, (rbn_sense,
+                set_of_lu_ids) in enumerate(rbn_sense_to_fn_lu.items(), 1):
         lu_ids = list(set_of_lu_ids)
         assert len(lu_ids) == 1
         lu_id = lu_ids[0]
@@ -90,18 +106,32 @@ def create_svg_folder_rbn_sense_to_fn_lus(output_folder,
         create_svg_from_rbn_sense_to_fn_frame(rbn_sense=sense_id_to_sense_label[rbn_sense],
                                               fn_lu=lu_id,
                                               g=g,
-                                              output_folder=output_folder,
+                                              index=index,
+                                              output_folder=images_folder,
                                               verbose=verbose)
-        svgs += 1
+        index_to_edge[index] = [rbn_sense, lu_id]
+        index_to_annotation[index] = False
+
+    assert len(index_to_edge) == len(rbn_sense_to_fn_lu), \
+        f'mismatch between input dict (size {len(sense_id_to_sense_label)}\
+          and number of svgs {len(index_to_edge)}'
+
+
+    for a_dict, path in [(index_to_edge, annotations_folder / 'id_to_edge.json'),
+                         (index_to_annotation, annotations_folder / 'annotations.json')]:
+        with open(path, 'w') as outfile:
+            json.dump(a_dict, outfile)
 
     if verbose >= 1:
         print()
         print(f'output folder: {output_folder}')
-        print(f'added {svgs} images to output folder.')
-
+        print(f'stored svgs in {images_folder}')
+        print(f'stored annotation input in {annotations_folder}')
+        print(f'added {len(index_to_edge)} images to output folder.')
 
 if __name__ == '__main__':
-    create_svg_folder_rbn_sense_to_fn_lus(output_folder='../output/annotation/v1',
-                                          graph_path='../output/dfn_objects/graph_v1.p',
-                                          path_rbn_to_lu='../development/rbn_senseid2fn_lu_ids.p',
-                                          verbose=1)
+    create_annotation_folder(output_folder='../output/iteration1/tool_input',
+                             graph_path='../output/dfn_objects/graph_v1.p',
+                             path_rbn_to_lu='../development/rbn_senseid2fn_lu_ids.p',
+                             overwrite=True,
+                             verbose=1)
