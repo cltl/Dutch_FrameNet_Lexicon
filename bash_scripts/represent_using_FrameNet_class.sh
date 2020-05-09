451e8e94c05fa3462a7786beef5afa806d1e1ba4
MartenPostma <martenp@gmail.com>
2020-05-09 22:55:46 +0200

    small bug fix

--- bash_scripts/represent_using_FrameNet_class.sh
+++ bash_scripts/represent_using_FrameNet_class.sh
@@ -2,5 +2,5 @@ export output=../output/dfn_objects
 mkdir -p $output 
 
 cd ../lib 
-python combine_resources.py --config_path="../input/config_files/v0.json" --output_folder=$output --use_cache="False" --verbose=1 > $output/representation.out 2> $output/representation.err &
+python combine_resources.py --config_path="../input/config_files/v0.json" --output_folder=$output --use_cache="False" --verbose=1 > $output/representation.out 2> $output/representation.err
 
